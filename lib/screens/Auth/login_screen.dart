9895d0b787a35d371431dcd838646b01854fd8d2
Hamza <hamzali4642@gmail.com>
2023-08-18 10:14:20 +0500

    auth

--- lib/screens/Auth/login_screen.dart
+++ lib/screens/Auth/login_screen.dart
@@ -1,7 +1,12 @@
 import 'package:admin_project/extras/colors.dart';
+import 'package:admin_project/extras/functions.dart';
+import 'package:admin_project/screens/Dashboard_screen.dart';
 import 'package:admin_project/widgets/custom_text.dart';
 import 'package:admin_project/widgets/text_field_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:utility_extensions/utility_extensions.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   @override
   late double width, height;
+
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
@@ -67,11 +73,14 @@ class _LoginScreenState extends State<LoginScreen> {
                   height: 10,
                 ),
                 SizedBox(
-                    height: height * 0.06,
-                    child: Center(
-                        child: TextFieldWidget(
-                            controller: emailController,
-                            hint: "Enter your password"))),
+                  height: height * 0.06,
+                  child: Center(
+                    child: TextFieldWidget(
+                      controller: passwordController,
+                      hint: "Enter your password",
+                    ),
+                  ),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -81,17 +90,56 @@ class _LoginScreenState extends State<LoginScreen> {
                           value: false,
                           onChanged: (v) {},
                         ),
-                        CustomText(text: "Remember me",fontSize: 14,fontWeight: FontWeight.w600,)
+                        CustomText(
+                          text: "Remember me",
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        )
                       ],
                     ),
-                    CustomText(text: "Forgot Password?",fontSize: 14,fontWeight: FontWeight.w600,)
+                    CustomText(
+                      text: "Forgot Password?",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ],
-                )
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 100,),
+                  ),
+                  onPressed: () {
+                    if(!emailController.text.isValidEmail){
+                      Functions.showSnackBar(context, "Please enter a valid email");
+                    }else if(passwordController.text.length < 8){
+                      Functions.showSnackBar(context, "Password should contain at least 8 characters.");
+                    }else{
+                      signin();
+                    }
+                  },
+                  child: Text("Login"),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  signin() {
+    Functions.showLoaderDialog(context);
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    )
+        .then((value) {
+      context.pushAndRemoveUntil(child: const DashBoardScreen());
+    }).catchError((error) {
+      context.pop();
+      FirebaseAuthException exception = error;
+      Functions.showSnackBar(context, exception.message ?? "");
+    });
   }
 }
